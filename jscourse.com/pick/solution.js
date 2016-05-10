00600ced9bef11000bfc43b3aa818d1e2aa9f696
re5pawn <s4ichulin@gmail.com>
2016-05-10 12:35:34 +0300

    add solution for pick task

---
+++ jscourse.com/pick/solution.js
@@ -0,0 +1,31 @@
+function pick(obj, keys) {
+	'use strict';
+
+	var assign = function(sourceObj, targetObj, key) {
+		if (sourceObj.hasOwnProperty(key)) {
+			targetObj[key] = sourceObj[key];
+		}
+
+		return targetObj;
+	};
+
+	var reduceFn = Array.prototype.reduce;
+	var handler = reduceFn && reduceFn.toString().indexOf('native') >= 0 ?
+		function() {
+			return keys.reduce(function(result, key) {
+				return assign(obj, result, key);
+			}, {});
+		} :
+		function() {
+			var result = {};
+			var i;
+
+			for (i = 0; i < keys.length; i++) {
+				assign(obj, result, keys[i]);
+			}
+
+			return result;
+		};
+
+		return handler();
+}
