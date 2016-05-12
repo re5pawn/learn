f4cae39169499d0aeb1c7e8a5f3c1d95d8a2e59a
re5pawn <s4ichulin@gmail.com>
2016-05-12 19:14:11 +0300

    remove log info

--- jscourse.com/compose/solution.js
+++ jscourse.com/compose/solution.js
@@ -3,13 +3,11 @@ function compose() {
 	var forEachFn = Array.prototype.forEach;
 	var handler = forEachFn && forEachFn.toString().indexOf('native') >= 0 ?
 		function() {
-			console.log('forEach');
 			fns.forEach(function(fn) {
 				fn();
 			});
 		} :
 		function() {
-			console.log('for');
 			var i;
 			for (i = 0; i < fns.length; i++) {
 				fns[i]();
