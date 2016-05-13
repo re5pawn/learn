5a8f6afa2195b6c140b9cc302ff2a7b1718395f1
re5pawn <s4ichulin@gmail.com>
2016-05-13 17:26:09 +0300

    add solution for find-longest-palindrome task

---
+++ jscourse.com/find-longest-palindrome/solution.js
@@ -0,0 +1,34 @@
+function findLongestPalindrome(stringToSearchIn) {
+	'use strict';
+
+	var isPalindrome = function(test) {
+		test = test.toLowerCase();
+		return test === test.split('').reverse().join('');
+	}
+
+	var str = stringToSearchIn
+		.toLowerCase()
+		.split('')
+		.filter(function(char) {
+			return char !== ' ';
+		})
+		.join('');
+
+	if (isPalindrome(str)) {
+		return str;
+	};
+
+	var length = str.length;
+	var result = '';
+	var i, test;
+
+	for (i = 0; i < length; i++) {
+		test = str.slice(0, i + 1);
+
+		if (isPalindrome(test)) {
+			result = test.length > result.length ? test : result;
+		}
+	}
+
+	return result;
+}
