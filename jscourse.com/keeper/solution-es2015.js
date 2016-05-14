eba03c82cc498c114f8a9757a777ea84c70d2d9f
re5pawn <s4ichulin@gmail.com>
2016-05-14 16:26:34 +0300

    add solution for keeper task

---
+++ jscourse.com/keeper/solution-es2015.js
@@ -0,0 +1,19 @@
+function createKeeper() {
+	let data = [];
+	
+	return {
+		put(key, value) {
+			if (data.some(d => d.key === key)) {
+				let d = data.filter(d => d.key === key)[0];
+				d.value = value;
+			} else {
+				data.push({ key, value });
+			}
+		},
+
+		get(key) {
+			let k = data.filter(d => d.key === key)[0];
+			return k ? k.value : null;
+		}
+	}
+}
