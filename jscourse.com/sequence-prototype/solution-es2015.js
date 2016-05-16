9e3ffc03410719a68f13caac0d2f9c7b67a24ee2
re5pawn <s4ichulin@gmail.com>
2016-05-16 12:24:21 +0300

    add es2015 solution for 'sequence-prototype' task

---
+++ jscourse.com/sequence-prototype/solution-es2015.js
@@ -0,0 +1,22 @@
+class Sequence {
+	constructor(arr) {
+		this.cursor = 0;
+		this.seq = arr;
+	}
+
+	go(index) {
+		const length = this.seq.length;
+		this.cursor = index >= length ? length - 1 : index;
+		return this.seq[this.cursor];
+	}
+
+	next() {
+		this.cursor = this.cursor + 1 >= this.seq.length ? 0 : this.cursor + 1;
+		return this.seq[this.cursor];
+	}
+
+	prev() {
+		this.cursor = this.cursor === 0 ? this.seq.length - 1 : this.cursor - 1;
+		return this.seq[this.cursor];
+	}
+}
