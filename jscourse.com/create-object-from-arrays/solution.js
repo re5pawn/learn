d5146eeb1db60ce9e57dd072b3a2c04efc0efeec
re5pawn <s4ichulin@gmail.com>
2016-05-25 19:38:16 +0300

    add solution for 'create-object-from-arrays' task

---
+++ jscourse.com/create-object-from-arrays/solution.js
@@ -0,0 +1,20 @@
+function createObject(arrOfKeys, arrOfData) {
+	return arrOfKeys.reduce(function(result, key, idx) {
+		result[key] = arrOfData[idx];
+		return result;
+	}, {});
+	
+	// or for older browsers
+	// var result = {};
+	// var length = arrOfKeys.length;
+	// var i, key, value;
+	
+	// for (i = 0; i < length; i++) {
+	// 	key = arrOfKeys[i];
+	// 	value = arrOfData[i];
+		
+	// 	result[key] = value;
+	// }
+	
+	// return result;
+}
