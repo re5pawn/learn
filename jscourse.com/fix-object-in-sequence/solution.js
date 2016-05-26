d69e3ef18766c2602f8a8bb3c2828b78dbdb79ce
re5pawn <s4ichulin@gmail.com>
2016-05-26 16:29:58 +0300

    add solution for 'fix-object-in-sequence' task

---
+++ jscourse.com/fix-object-in-sequence/solution.js
@@ -0,0 +1,30 @@
+function fixStruct(struct) {
+	// we don't need the first and the last element
+	var i = 1;
+	var length = struct.length - 1;
+
+	var curr, next, prev;
+	var defineVariables = function(idx) {
+		curr = struct[idx];
+		next = struct[idx + 1];
+		prev = struct[idx - 1];
+	}
+
+	for (i; i < length; i++) {
+		defineVariables(i);
+
+		// find the incorrect value of sequence
+		if (curr.value !== next.value - next.delta) {
+
+			// we need only numbers
+			if (isNaN(next.value) || isNaN(next.delta)) {
+				// go to the next sequence item
+				i++;
+				defineVariables(i);
+			}
+
+			curr.value = next.value - next.delta;
+			curr.delta = curr.value - prev.value;
+		}
+	}
+}
