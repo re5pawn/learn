2641d78f57b475c12fa25b09a6fb6cfde9871e31
re5pawn <s4ichulin@gmail.com>
2016-05-29 14:55:11 +0300

    add solution for 'simple-templater' task

---
+++ jscourse.com/simple-templater/solution.js
@@ -0,0 +1,15 @@
+function templater(templateString, dataObj) {
+	var startIdx, endChar, re, key;
+
+	for (key in dataObj) {
+		startIdx = templateString.indexOf('${' + key);
+		endChar = templateString[startIdx + key.length + 2]; // 2 === '${'.length
+		re = new RegExp('\\${' + key + '}', 'g'); // /\${key}/g
+
+		if (startIdx >= 0 && endChar === '}') {
+			templateString = templateString.replace(re, dataObj[key]);
+		}
+	}
+
+	return templateString;
+}
