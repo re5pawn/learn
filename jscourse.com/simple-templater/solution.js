e5625c24a1fe10709198c5a4ff856205fbddc6c0
re5pawn <s4ichulin@gmail.com>
2016-05-31 14:09:51 +0300

    change solution for 'simple-templater' task

--- jscourse.com/simple-templater/solution.js
+++ jscourse.com/simple-templater/solution.js
@@ -1,16 +1,11 @@
 function templater(templateString, dataObj) {
 	'use strict';
 
-	var startIdx, endChar, re, key;
+	var key, re;
 
 	for (key in dataObj) {
-		startIdx = templateString.indexOf('${' + key);
-		endChar = templateString[startIdx + key.length + 2]; // 2 === '${'.length
 		re = new RegExp('\\${' + key + '}', 'g'); // /\${key}/g
-
-		if (startIdx >= 0 && endChar === '}') {
-			templateString = templateString.replace(re, dataObj[key]);
-		}
+		templateString = templateString.replace(re, dataObj[key]);
 	}
 
 	return templateString;
