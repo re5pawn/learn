5c862a67d76a4a5ebed6d9d619193e04ac7a4504
re5pawn <s4ichulin@gmail.com>
2016-06-09 14:25:54 +0300

    added solution for 'reverseInt' task

---
+++ misc/reverseInt/solution.js
@@ -0,0 +1,10 @@
+function reverseInt(n) {
+	'use strict';
+	
+	if (isNaN(n) || typeof n !== 'number') {
+		throw new Error('Parameter must be a number');
+	}
+
+	var rn = parseInt(n.toString().split('').reverse().join(''), 10);
+	return n < 0 ? -rn : rn;
+}
