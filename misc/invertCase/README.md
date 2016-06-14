e4421da7eb60a589d90f6a4803554c0dbcc4b633
re5pawn <s4ichulin@gmail.com>
2016-06-14 14:58:15 +0300

    added solution for 'invertCase' task

---
+++ misc/invertCase/README.md
@@ -0,0 +1,8 @@
+## Invert case
+
+Реализуйте функцию `invertCase`, которая меняет в строке регистр каждой буквы, на противоположный.
+
+```js
+invertCase('Hello, World!') // hELLO, wORLD!
+invertCase('I loVe JS') // i LOvE js
+```
---
+++ misc/invertCase/solution-es2015.js
@@ -0,0 +1,12 @@
+const invertCase = (string) => {
+	let toLow, toUp;
+
+	return string.split('').reduce((result, letter) => {
+		toLow = letter.toLowerCase();
+		toUp = letter.toUpperCase();
+
+		letter = toLow === letter ? toUp : toLow;
+
+		return result += letter;
+	}, '');
+}
