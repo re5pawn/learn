e69960f4e0ba45a02dc3c13ee1e771cae41943ce
re5pawn <s4ichulin@gmail.com>
2016-06-18 18:04:48 +0300

    added solution for 'Exes and Ohs' quiz

---
+++ misc/exesAndOhs/solution-es2015.js
@@ -0,0 +1,12 @@
+function XO(str) {
+	str = str.trim().toLowerCase();
+
+	let count = 0;
+
+	for (let char of str) {
+		if (char === 'x') { count++; }
+		if (char === 'o') { count--; }
+	}
+
+	return count === 0;
+}
