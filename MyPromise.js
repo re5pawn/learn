c8662183ab6bfc3919cf0283704f6067367b2b02
Sergey Chichulin <re5pawn@users.noreply.github.com>
2016-06-20 22:57:05 +0300

    an initial commit for MyPromise

---
+++ MyPromise.js
@@ -0,0 +1,54 @@
+// An implementation of Promise A+ using JavaScript
+// In progress...
+
+function MyPromise(fnAsync) {
+	var resolvers = [];
+	var rejecters = [];
+	var data;
+	var error;
+
+	var resolved = function(d) {
+		data = d;
+
+		return resolvers.reduce(function(result, resolve) {
+			return resolve(result);
+		}, data);
+	};
+
+	var rejected = function(err) {
+		console.error('rejected', err);
+
+		error = err;
+		// will be later...
+	};
+
+	fnAsync(resolved, rejected);
+
+	this.then = function(fn) {
+		if (resolvers.indexOf(fn) < 0) {
+			resolvers.push(fn);
+		}
+		console.log('resolvers list:', resolvers);
+
+		return this;
+	}
+}
+
+// usage example
+var p = new MyPromise((success, fail) => {
+	var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+	setTimeout(() => {
+		if (Math.random() < 0.5) {
+			success(arr);
+		} else {
+			fail(new Error('fail from MyPromise'));
+		}
+	}, 3000);
+});
+
+var log = (d) => console.log('log:', d);
+var firstFilter = (arr) => arr.filter(el => el >= 5);
+var secondFilter = (arr) => arr.filter(el => el >= 7);
+
+p.then(firstFilter).then(secondFilter).then(log); // after 3 sec -> [7, 8, 9, 10]
