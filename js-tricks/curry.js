624a6129351ec27c9675c26382c82a0db8e83df6
re5pawn <s4ichulin@gmail.com>
2016-06-27 13:36:08 +0300

    updated 'curry' and added usage examples

--- js-tricks/curry.js
+++ js-tricks/curry.js
@@ -1,18 +1,57 @@
-function curry(fn) {
-  var slice = Array.prototype.slice;
-  var oldArgs = slice.call(arguments, 1);
-  
-  return function() {
-    var newArgs = slice.call(arguments);
-    var args = oldArgs.concat(newArgs);
-    return fn.apply(null, args);
+function _curry(mode) {
+  // mode = 'left' || 'right'
+
+  return function(fn) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    
+    return function(newArg) {
+      if (arguments.length > 1) {
+        throw new Error('Function is expecting only one parameter');
+      }
+
+      var newArgs = mode === 'right'
+        ? [newArg].concat(args)
+        : args.concat([newArg]);
+
+      return fn.apply(null, newArgs);
+    }
   }
 }
 
+var curry = _curry('left');
+var curryRight = _curry('right');
+
 // Usage
-function sum(a, b, c) {
-  return a + b + c;
+function setBgColor(node, color) {
+  console.log(arguments);
+  node.style.backgroundColor = color;
 }
 
-var newSum = curry(sum, 1, 2);
-newSum(3); // 6
+// Example 01: set the same background color for different elements
+var setGreenBg = curryRight(setBgColor, 'lightgreen');
+
+var someElement = document.querySelector('.some-element');
+var anotherElement = document.querySelector('.another-element');
+var elementN = document.querySelector('.elementN');
+
+setGreenBg(someElement);
+// someElement will have lightgreen background color
+
+setGreenBg(anotherElement);
+// anotherElement will have lightgreen background color
+
+setGreenBg(elementN);
+// element2 will have lightgreen background color
+
+// Example 02: set a different background color for the same element
+var someButton = document.querySelector('.some-button');
+var setButtonBgColor = curry(setBgColor, someButton);
+
+setButtonBgColor('lightblue');
+// someButton will have lightblue background color
+
+setButtonBgColor('lightgreen');
+// someButton will have lightgreen background color
+
+setButtonBgColor('red');
+// someButton will have red background color
