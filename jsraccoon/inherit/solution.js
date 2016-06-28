1806230b1789a07a82420d75a93e60618795270e
re5pawn <s4ichulin@gmail.com>
2016-06-28 15:06:31 +0300

    rm some code

--- jsraccoon/inherit/solution.js
+++
@@ -1,8 +0,0 @@
-function inherit(child, parent) {
-  'use strict';
-
-  Object.keys(parent.prototype).forEach(function(key) {
-    if (typeof parent.prototype[key] !== 'function') { return; }
-    child.prototype[key] = parent.prototype[key];
-  });
-}
